6f4cf5d6a933cc5aa1cf9d2ed72c6dfc330b085c
James Stephens <stephensja7@gmail.com>
2018-06-03 04:24:15 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+=== Reading with Dragons ===
+
+Requires at least: 4.9.4
+Tested up to: 4.9.4
+Stable tag: 1.0.0
+
+== Description ==
+
+Reading with Dragons is a young adults book blog, by Kelly Stephens, with a primary focus on musings about YA books, heroines, and culture.
+
+== Installation ==
+
+1. In your admin panel, go to Appearance > Themes and click the Add New button.
+2. Click Upload Theme and Choose File, then select the theme's .zip file. Click Install Now.
+3. Click Activate to use your new theme right away.
+
+== Frequently Asked Questions ==
+
+= Does this theme support any plugins? =
+
+Reading with Dragons includes support for Infinite Scroll in Jetpack.
+
+== Changelog ==
+
+= 1.0 - February 11 2018 =
+* Initial release
---
+++ assets/template/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Reading_with_Dragons
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'readingwithdragons' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'readingwithdragons' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'readingwithdragons' ), 'readingwithdragons', '<a href="https://jstoobz.com/">James Stephens</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
