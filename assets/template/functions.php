c54eb4ced95543d69b23cdcc809a007d8cf56908
James Stephens <stephensja7@gmail.com>
2018-08-07 23:53:31 -0400

    updated typography and added foundations for reson for responsive layouts

--- assets/template/functions.php
+++ assets/template/functions.php
@@ -84,6 +84,59 @@ endif;
 add_action( 'after_setup_theme', 'readingwithdragons_setup' );
 
 /**
+ * Register custom fonts.
+ */
+function readingwithdragons_fonts_url() {
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by Quicksand, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$quicksand = _x( 'on', 'Quicksand font: on or off', 'readingwithdragons' );
+
+	$font_families = array();
+
+	if ( 'off' !== $quicksand ) {
+		$font_families[] = 'Quicksand:500,700';
+	}
+
+	if ( in_array( 'on', array($quicksand) ) ) {
+
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+			'subset' => urlencode( 'latin,latin-ext' ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, 'https://fonts.googleapis.com/css' );
+	}
+
+	return esc_url_raw( $fonts_url );
+}
+
+/**
+ * Add preconnect for Google Fonts.
+ *
+ * @since Twenty Seventeen 1.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function readingwithdragons_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'readingwithdragons-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'readingwithdragons_resource_hints', 10, 2 );
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
@@ -120,11 +173,13 @@ add_action( 'widgets_init', 'readingwithdragons_widgets_init' );
  * Enqueue scripts and styles.
  */
 function readingwithdragons_scripts() {
+	wp_enqueue_style( 'readingwithdragons-fonts', readingwithdragons_fonts_url() );
+
 	wp_enqueue_style( 'readingwithdragons-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'readingwithdragons-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'readingwithdragons-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20180807', true );
 
-	wp_enqueue_script( 'readingwithdragons-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'readingwithdragons-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20180807', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
